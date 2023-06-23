6489528887e298c59f08da02b3a232e70a7216f7
HuTao <gongbo@hotmail.com>
2023-06-23 16:29:37 +0800

    init

---
+++ src/test/java/com/liangcha/LiangchaYudaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liangcha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiangchaYudaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
